1ae1b8c88a958dcbc74491bc5427e90530c5cce6
Will <u9734017@gmail.com>
2015-09-01 21:48:28 +0800

    first-commit

---
+++ fruitpay/src/main/webapp/js/main.js
@@ -0,0 +1,8 @@
+angular.module('myApp', ['mgcrea.ngStrap']);
+/*	.controller('DemoCtrl', function($scope, $modal) {
+		var myModal = $modal({title: 'My Title', content: 'My Content', show: false});
+		$scope.showModal = function() {
+			myModal.$promise.then(myModal.show);
+		};
+	})
+*/
