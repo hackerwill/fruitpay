a1a5d85b065b034ef53311a52848daa19551989c
Will <u9734017@gmail.com>
2015-09-10 23:24:26 +0800

    local change

--- fruitpay/src/main/java/com/fruitpay/base/controller/BaseController.java
+++ fruitpay/src/main/java/com/fruitpay/base/controller/BaseController.java
@@ -1,27 +1,18 @@
 package com.fruitpay.base.controller;
 
-import java.util.List;
-
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.fruitpay.base.comm.PageConst;
-import com.fruitpay.base.dao.TestDao;
-import com.fruitpay.base.model.Test;
 
 @Controller
 public class BaseController {
 	
 	private final org.slf4j.Logger logger = LoggerFactory.getLogger(this.getClass());
 	private static int counter = 0;
-	
-	@Autowired
-	TestDao testDao;
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String welcome(ModelMap model) {
@@ -29,8 +20,6 @@ public class BaseController {
 		model.addAttribute("message", "Welcome");
 		model.addAttribute("counter", ++counter);
 		logger.debug("[welcome] counter : {}", counter);
-		
-		List<Test> rs = testDao.getAllTask();
 
 		// Spring uses InternalResourceViewResolver and return back index.jsp
 		return PageConst.MAIN_PAGE.toString();
