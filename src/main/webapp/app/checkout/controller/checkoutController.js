b9182a8c33f215c9662750c69cdb72972c2bf133
Will <u9734017@gmail.com>
2015-10-27 07:49:20 +0800

    modify checkout page

--- src/main/webapp/app/checkout/controller/checkoutController.js
+++ src/main/webapp/app/checkout/controller/checkoutController.js
@@ -5,32 +5,24 @@ angular.module('checkout')
 		
 		//bind scroll up and down event
 		angular.element($window).bind('DOMMouseScroll mousewheel onmousewheel', function(event) {
-			
+				event.returnValue = false;
+				// for Chrome and Firefox
+				if(event.preventDefault) {
+					event.preventDefault();                        
+				}
 				// cross-browser wheel delta
 				var event = window.event || event; // old IE support
 				var delta = Math.max(-1, Math.min(1, (event.wheelDelta || -event.detail)));
-                
+				
 				if(delta > 0) {
-					event.returnValue = false;
-					// for Chrome and Firefox
-					if(event.preventDefault) {
-						event.preventDefault();                        
-					}
 					scrollElement.scrollToPrevious();   
 				}else{
-					event.returnValue = false;
-					// for Chrome and Firefox
-					if(event.preventDefault) {
-						event.preventDefault();                        
-					}
 					scrollElement.scrollToNext();
-				}
+				}	
 		});
 		
 		function scrollElement(){
 			var onePageElements = document.getElementsByClassName("onepageElement");
-			
-			
 			var returnObj = {};
 			
 			returnObj.scrollToNext = scrollToNext;
@@ -64,12 +56,26 @@ angular.module('checkout')
 				var currentElement = onePageElements[activeIndex];
 				var nextElement = onePageElements[activeIndex+i];
 				if(nextElement){
-					currentElement.className = currentElement.className.replace("active", " ");
 					activeIndex = activeIndex + i;
-					nextElement.className = nextElement.className + " active";
 					$document.scrollToElementAnimated(onePageElements[activeIndex]);
+					//set timeout to prevent the scroll event trigger many times
+					setTimeout(function(){
+						for(var i = 0; i < onePageElements.length ; i++){
+							onePageElements[i].className = 
+							replaceAll("active|\sactive", "", onePageElements[i].className);
+						}
+						nextElement.className = nextElement.className.trim();
+						if(nextElement.className.indexOf("active") == -1)
+							nextElement.className = nextElement.className + " active";
+							
+					}, 1000);
 				}
 			}
+			
+			function replaceAll(find, replace, str) {
+				return str.replace(new RegExp(find, 'g'), replace);
+			}
+
 		}
 		
 	}]);
