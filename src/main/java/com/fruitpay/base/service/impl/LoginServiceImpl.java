a6b8318a127c4fd8f14d5b7e73705327d32d220e
Will <u9734017@gmail.com>
2015-12-12 12:05:56 +0800

    before merge

--- src/main/java/com/fruitpay/base/service/impl/LoginServiceImpl.java
+++ src/main/java/com/fruitpay/base/service/impl/LoginServiceImpl.java
@@ -1,8 +1,5 @@
 package com.fruitpay.base.service.impl;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
