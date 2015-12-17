e7a17456b3b0bfbe897d959379e751db34743f44
Bruce <weichou1229@gmail.com>
2015-12-18 00:01:06 +0800

    升級spring到4.2.3

--- src/main/java/com/fruitpay/base/controller/CustomerDataController.java
+++ src/main/java/com/fruitpay/base/controller/CustomerDataController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +23,7 @@ import com.fruitpay.base.service.CustomerService;
 import com.fruitpay.comm.utils.AssertUtils;
 import com.fruitpay.comm.utils.AuthenticationUtil;
 import com.fruitpay.comm.utils.Md5Util;
-
+@CrossOrigin(origins = "http://localhost:8888", maxAge = 3600)
 @Controller
 @RequestMapping("customerDataCtrl")
 public class CustomerDataController {
