799f89265214e5de56f96f70ed15d5a1a0ce1954
Will <u9734017@gmail.com>
2016-01-16 16:50:13 +0800

    add backend page setting

--- src/main/resources/sh/JenkinsShellAfterBuild.sh
+++ src/main/resources/sh/JenkinsShellAfterBuild.sh
@@ -1,2 +1,2 @@
 mvn tomcat7:undeploy -Dtomcat.username=will -Dtomcat.password=tomcatWillServer
-mvn tomcat7:deploy -Dtomcat.username=will -Dtomcat.password=tomcatWillServer
+mvn tomcat7:deploy -Dmaven.test.skip=true -Dtomcat.username=will -Dtomcat.password=tomcatWillServer
