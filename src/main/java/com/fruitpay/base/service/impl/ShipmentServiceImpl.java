f748055ab1d0b85bdd2220fd8609edfe482910f3
Will Hsu <will.g@mig.me>
2016-05-21 11:54:17 +0800

    remove unneccesrry import to avoid problme

--- src/main/java/com/fruitpay/base/service/impl/ShipmentServiceImpl.java
+++ src/main/java/com/fruitpay/base/service/impl/ShipmentServiceImpl.java
@@ -9,18 +9,15 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
-import org.apache.commons.lang.time.DateUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.BeanUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
