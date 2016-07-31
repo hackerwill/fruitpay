8f26bababdd4a529452db0a28b7276dc1b7d8c9f
wowmama <wowmama0107@gmail.com>
2016-07-31 10:29:59 +0800

    Modify TaskScheduler.java, add postInvoice() method.

--- src/main/java/com/fruitpay/base/service/impl/TaskScheduler.java
+++ src/main/java/com/fruitpay/base/service/impl/TaskScheduler.java
@@ -11,13 +11,15 @@ import org.apache.log4j.Logger;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.fruitpay.allpayInvoice.constants.RtnCode;
 import com.fruitpay.base.dao.ScheduledRecordDAO;
-import com.fruitpay.base.model.CachedBean;
-import com.fruitpay.base.model.CustomerOrder;
+import com.fruitpay.base.model.Invoice;
 import com.fruitpay.base.model.ScheduledRecord;
 import com.fruitpay.base.model.ShipmentRecord;
+import com.fruitpay.base.service.AllPayService;
 import com.fruitpay.base.service.CachedService;
 import com.fruitpay.base.service.CustomerOrderService;
+import com.fruitpay.base.service.InvoiceService;
 import com.fruitpay.base.service.ShipmentService;
 import com.fruitpay.comm.utils.DateUtil;
 
@@ -40,6 +42,10 @@ public class TaskScheduler {
 	private CachedService cachedService;
 	@Inject
 	private ScheduledRecordDAO scheduledRecordDAO;
+	@Inject
+	private InvoiceService invoiceService;
+	@Inject
+	private AllPayService allPayService;
 	
 	@Scheduled(cron="0 0 6-23 * * *")
 	public void calulcateShipmentData() {
@@ -72,6 +78,10 @@ public class TaskScheduler {
 			shipmentRecord = shipmentService.add(shipmentRecord, orderIds);
 			scheduledRecord.setIsSuccessful(IsSuccessful.Y.name());
 			scheduledRecord.setMessage("Record added, shipmentRecordId: " + shipmentRecord.getShipmentRecordId());
+			
+			//每筆ShipmentRecoidDetail都要開一筆發票資訊。
+			shipmentRecord.getShipmentRecordDetails().forEach((v)->invoiceService.add(v));
+			
 		} catch(Exception e) {
 			logger.error(e);
 			scheduledRecord.setIsSuccessful(IsSuccessful.N.name());
@@ -81,6 +91,37 @@ public class TaskScheduler {
 			scheduledRecordDAO.save(scheduledRecord);
 		}
 		
+	}
+	
+	@Scheduled(cron="0 30 22 * * *")
+	public void postInvoice(){
+		logger.debug("Enter TaskScheduler.postInvoice");
+		List<Invoice> invoices = invoiceService.findUnpostInvoice();
+		for(Invoice invoice : invoices){
+			ScheduledRecord scheduledRecord = new ScheduledRecord();
+			scheduledRecord.setMethodName("postInvoice");
+			try {
+				
+				Map<String, String> response = allPayService.postInvoice(invoice);
+				String rtnCode = response.get("RtnCode");
+				
+				if(RtnCode.SUCCESSED.equals(rtnCode)){
+					invoiceService.updateInvoiceSuccessed(invoice);
+				}else{
+					invoiceService.updateInvoiceFailed(invoice);
+				}
+				
+				scheduledRecord.setIsSuccessful(IsSuccessful.Y.name());
+				scheduledRecord.setMessage("Record added, invoiceId: " + invoice.getInvoiceId());
+			} catch(Exception e) {
+				logger.error(e);
+				scheduledRecord.setIsSuccessful(IsSuccessful.N.name());
+				scheduledRecord.setMessage(getErrorMessage(e));
+			} finally {
+				scheduledRecord.setCreateDate(new Date());
+				scheduledRecordDAO.save(scheduledRecord);
+			}
+		}
 	}
 	
 	private String getErrorMessage(Exception e) {
