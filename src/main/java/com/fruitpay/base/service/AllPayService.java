88231fd73d7bf4a6e05cee83c3b214344ba52c25
wowmama <wowmama0107@gmail.com>
2016-07-31 10:29:18 +0800

    Modify AllpayService. Add postInvoice method.

--- src/main/java/com/fruitpay/base/service/AllPayService.java
+++ src/main/java/com/fruitpay/base/service/AllPayService.java
@@ -1,5 +1,15 @@
 package com.fruitpay.base.service;
 
-public interface AllPayService {
+import java.util.Map;
 
+import com.fruitpay.base.model.Invoice;
+import com.fruitpay.base.model.ShipmentRecordDetail;
+
+public interface AllPayService {
+	/**
+	 * 向Allpay開立發票。
+	 * @param invoice fruitpay內部發票。
+	 * @return Map<String, String> allpay回傳參數
+	 */
+	public Map<String, String> postInvoice(Invoice invoice);
 }
--- src/main/java/com/fruitpay/base/service/impl/AllpayServiceImpl.java
+++ src/main/java/com/fruitpay/base/service/impl/AllpayServiceImpl.java
@@ -1,23 +1,80 @@
 package com.fruitpay.base.service.impl;
 
-import java.util.ArrayList;
 import java.util.Date;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.ToIntFunction;
 
+import org.springframework.stereotype.Service;
+
+import com.fruitpay.allpayInvoice.machine.InvoiceMachine;
+import com.fruitpay.allpayInvoice.machine.MachineFactory;
+import com.fruitpay.allpayInvoice.machine.MachineType;
+import com.fruitpay.allpayInvoice.model.AllpayInvoice;
+import com.fruitpay.allpayInvoice.util.AllpayURLEncoder;
+import com.fruitpay.base.model.Customer;
+import com.fruitpay.base.model.CustomerOrder;
+import com.fruitpay.base.model.Invoice;
+import com.fruitpay.base.model.InvoiceDetail;
+import com.fruitpay.base.model.OrderProgram;
+import com.fruitpay.base.model.ShipmentRecordDetail;
 import com.fruitpay.base.service.AllPayService;
 
-import AllPay.Payment.Integration.AllInOne;
-import AllPay.Payment.Integration.Decimal;
-import AllPay.Payment.Integration.DeviceType;
-import AllPay.Payment.Integration.ExtraPaymentInfo;
-import AllPay.Payment.Integration.HttpMethod;
-import AllPay.Payment.Integration.Item;
-import AllPay.Payment.Integration.PaymentMethod;
-import AllPay.Payment.Integration.PaymentMethodItem;
-import AllPay.Payment.Integration.PeriodType;
-
+@Service
 public class AllpayServiceImpl implements AllPayService {
 
-	
+	private final String TEST_HASH_KEY = "ejCk326UnaZWKisg";
+	private final String TEST_HASH_IV = "q9jcZX8Ib9LM8wYk";
+	private final String TEST_MERCHANT_ID = "2000132";
+
+	@Override
+	public Map<String, String> postInvoice(Invoice invoice) {
+		
+		Map<String, String>  response = new LinkedHashMap<String, String>();
+		
+		InvoiceMachine invoiceMachine = MachineFactory.getInvoiceMachine(MachineType.CREATE)
+				.setHashIV(TEST_HASH_IV)
+				.setHashKey(TEST_HASH_KEY)
+				.setMerchantId(TEST_MERCHANT_ID);
+		
+		ShipmentRecordDetail shipmentRecordDetail = invoice.getShipmentRecordDetail();
+		CustomerOrder customerOrder = shipmentRecordDetail.getCustomerOrder();
+		Customer customer = customerOrder.getCustomer();
+		List<InvoiceDetail> invoiceDetails = invoice.getInvoiceDetail();
+		int salesAmount = invoiceDetails
+				.stream()
+				.mapToInt(i -> i.getItemCount() * i.getItemPrice())
+				.sum();
+		
+		AllpayInvoice allpayInvoice = new AllpayInvoice()
+				.setRelateNumber(invoice.getRelateNumber())
+				.setTaxType(AllpayInvoice.TaxTypeEnum.DUTY_FREE)
+				.setSalesAmount(salesAmount)
+				.setTimeStamp(new Date());
+		
+		allpayInvoice.getCustomer()
+			.setCustomerName(customer.getLastName() + customer.getFirstName())
+			.setCustomerAddr(customer.getAddress())
+			.setCustomerEmail(customer.getEmail())
+			.setCustomerId(customer.getCustomerId().toString())
+			.setCustomerPhone(customer.getCellphone());
+		
+		for(InvoiceDetail invoiceDetail : invoiceDetails){
+			allpayInvoice.createItem()
+				.setItemName(invoiceDetail.getItemName())
+				.setItemCount(invoiceDetail.getItemCount())
+				.setItemPrice(invoiceDetail.getItemPrice())
+				.setItemWord(invoiceDetail.getItemWord())
+				.setItemRemark(invoiceDetail.getItemRemark());
+		}
+		
+		try {
+			response.putAll(invoiceMachine.post(allpayInvoice));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return response;
+	}
 
 }
